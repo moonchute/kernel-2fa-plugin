481fb1987acdb93f746f32268fdeb12e44c06007
leekt <leekt216@gmail.com>
2023-04-11 19:32:37 +0900

    v0.5 deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
+import * as dotenv from 'dotenv'
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-foundry";
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-deploy";
 import '@typechain/hardhat'
+dotenv.config()
+
+
+function getAccounts (): string[] | { mnemonic: string} {
+  const accs = []
+  if (process.env.DEPLOYER_PRIVATE_KEY !== undefined) {
+    accs.push(process.env.DEPLOYER_PRIVATE_KEY)
+  }
+  if (process.env.PAYMASTER_OWNER_PRIVATE_KEY !== undefined) {
+    accs.push(process.env.PAYMASTER_OWNER_PRIVATE_KEY)
+  }
+  return accs
+}
 
 const config: HardhatUserConfig = {
   solidity: {
